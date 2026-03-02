f004b2558904d585460812a566bcd0fe63ae4c96
Sérgio Alves <sergioalves.194@gmail.com>
2026-03-02 18:17:08 +0000

    Replace Modal with ImageModal for full-screen images

--- src/app/publications/[id]/page.js
+++ src/app/publications/[id]/page.js
@@ -201,72 +201,12 @@ export default function Details() {
 				</Stack>
 			</Container>
 
-			<Modal
-				aria-labelledby="full-screen-image-modal"
-				aria-describedby="full-screen-image-description"
+			<ImageModal
 				open={openFullScreen}
 				onClose={handleCloseFullScreen}
-				closeAfterTransition
-				slots={{ backdrop: Backdrop }}
-				slotProps={{
-					backdrop: {
-						timeout: 500,
-					},
-				}}
-				sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-			>
-				<Fade in={openFullScreen}>
-					<Box
-						sx={{
-							outline: 'none',
-							position: 'relative',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							bgcolor: 'transparent',
-							borderRadius: 2,
-							width: 'auto',
-							height: 'auto',
-							maxWidth: '70vw',
-							maxHeight: '70vh',
-						}}
-					>
-
-						<IconButton
-							aria-label="close"
-							size="large"
-							autoFocus
-							onClick={handleCloseFullScreen}
-							sx={{
-								position: 'absolute',
-								top: 8,
-								right: 8,
-								color: 'white',
-								zIndex: 1,
-								backgroundColor: 'rgba(0, 0, 0, 0.5)',
-								borderRadius: '50%',
-								'&:hover': {
-									backgroundColor: 'rgba(0, 0, 0, 0.7)',
-								},
-								boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-							}}
-						>
-							<CloseIcon />
-						</IconButton>
-
-						{currentImage && (
-							<img
-								src={currentImage}
-								alt={currentAltText}
-								style={{
-									maxWidth: 'inherit',
-									maxHeight: 'inherit'
-								}}
-							/>
-						)}
-					</Box>
-				</Fade>
-			</Modal>
+				imageUrl={currentImage}
+				altText={currentAltText}
+			/>
 
 		</Box>
 	);
